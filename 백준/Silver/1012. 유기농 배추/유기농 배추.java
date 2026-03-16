29743f926ba8b57ba89055a2435f9f06929efc6d
Hong5175 <sony51755175@gmail.com>
2026-03-16 11:18:35 +0900

    [Silver II] Title: 유기농 배추, Time: 148 ms, Memory: 16076 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -5,70 +5,70 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
 	public static void main(String[] args) throws IOException{
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		for(int z = 0; z < T; z++) {
+		int Test_case = Integer.parseInt(br.readLine());
+			
+		for(int p = 0; p < Test_case; p++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
+			
 			int m = Integer.parseInt(st.nextToken());
 			int n = Integer.parseInt(st.nextToken());
-			int k = Integer.parseInt(st.nextToken());
+			int t = Integer.parseInt(st.nextToken());
 			
 			int board[][] = new int[n][m];
-			for(int j = 0; j < k; j++) {
-				StringTokenizer sb = new StringTokenizer(br.readLine());
-				int y = Integer.parseInt(sb.nextToken());
-				int x = Integer.parseInt(sb.nextToken());
-				board[x][y] = 1;		
+			
+			for(int i = 0; i < t; i++) {
+				st = new StringTokenizer(br.readLine());
+				int y = Integer.parseInt(st.nextToken());
+				int x = Integer.parseInt(st.nextToken());
+				
+				board[x][y] = 1;
 			}
+			
 //			for(int i = 0; i < n; i++) {
-//				for(int j = 0; j < m; j++) {
+//				for(int j = 0 ; j < m; j++) {
 //					System.out.print(board[i][j] + " ");
 //				}
 //				System.out.println();
-//				
 //			}
-			
-			
-			int flag[][] = new int[n][m];
+//			System.out.println();
+			int count = 0;
 			int dx[] = {-1, 0, 1, 0};
 			int dy[] = {0, 1, 0, -1};
 			
-			Deque<int[]> queue = new ArrayDeque<>();
-			int count = 0;
+			boolean visited[][] = new boolean[n][m];
+			
 			for(int i = 0; i < n; i++) {
-				for(int j = 0; j < m; j++) {
-					if(board[i][j] == 1 && flag[i][j] == 0) {
-						queue.add(new int[] {i, j});
-						flag[i][j] = 1;
-						count += 1;
+				for(int j = 0 ; j < m; j++) {
+					if(visited[i][j] == true) continue;
+					if(board[i][j] == 0) continue;
+					
+					visited[i][j] = true;
+					Deque<int[]> queue = new ArrayDeque<>();
+					queue.add(new int[] {i, j});
+					count++;
+					while(!queue.isEmpty()) {
+						int num[] = queue.poll();
+						int x = num[0];
+						int y = num[1];
 						
-						while(!queue.isEmpty()) {
-							int num[] = queue.poll();
-							int x = num[0];
-							int y = num[1];
+						for(int a = 0 ; a < 4; a++) {
+							int nx = x + dx[a];
+							int ny = y + dy[a];
+							if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+							if(board[nx][ny] == 0) continue;
+							if(visited[nx][ny] == true) continue;
 							
-							for(int t = 0; t < 4; t++) {
-								int nx = x + dx[t];
-								int ny = y + dy[t];
-								
-								if(nx >= 0 && nx < n && ny >= 0  && ny < m) {
-									if(flag[nx][ny] == 0 && board[nx][ny] == 1) {
-										flag[nx][ny] = 1;
-										queue.add(new int[] {nx, ny});
-									}
-								}
-							}
+							queue.add(new int[] {nx, ny});
+							visited[nx][ny] = true;
 						}
 					}
 				}
-				
 			}
 			System.out.println(count);
-			
-			
 		}
-		
 	}
 }
