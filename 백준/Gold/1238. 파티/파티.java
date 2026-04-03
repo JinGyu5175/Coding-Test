6147daebd03ce8d2a2f94a4094a4b41ee090b8f5
Hong5175 <sony51755175@gmail.com>
2026-04-03 14:19:55 +0900

    [Gold III] Title: 파티, Time: 180 ms, Memory: 19124 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,43 +6,50 @@ import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		int x = Integer.parseInt(st.nextToken());
-		int INF = Integer.MAX_VALUE;
-		List<int[]>graph[] = new ArrayList[n + 1];
-		List<int[]>graph2[] = new ArrayList[n + 1];
+		List<int[]> graph[] = new ArrayList[n + 1];
+		List<int[]> graph2[] = new ArrayList[n + 1];
 		
-		int dist[] = new int[n + 1];
-		int dist2[] = new int[n + 1];
 		for(int i = 1; i < n + 1; i++) {
 			graph[i] = new ArrayList<>();
-			dist[i] = INF;
 			graph2[i] = new ArrayList<>();
-			dist2[i] = INF;
 		}
 		
 		for(int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
+			int time = Integer.parseInt(st.nextToken());
 			
-			graph[to].add(new int[] {cost, from});
-			graph2[from].add(new int[] {cost, to});
+			graph[from].add(new int[] {time, to});
+			graph2[to].add(new int[] {time, from});
+		}
+		
+		int dist[] = new int [n + 1];
+		int dist2[] = new int [n + 1];
+		
+		int INF = Integer.MAX_VALUE;
+		
+		for(int i = 1; i < n + 1; i++) {
+			dist[i] = INF;
+			dist2[i] = INF;
 		}
 		
 		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-		pq.add(new int[] {0, x});
+		// x에서 다른 곳으로 가는 길이
 		dist[x] = 0;
+		pq.add(new int[] {0, x});
+		
 		while(!pq.isEmpty()) {
-			int num[] = pq.poll();
-			int cost = num[0];
-			int v = num[1];
+			int cur[] = pq.poll();
+			int cost = cur[0];
+			int v = cur[1];
 			
 			if(dist[v] < cost) continue;
 			
@@ -54,18 +60,24 @@ public class Main{
 				int newCost = cost + nextCost;
 				if(dist[nextV] > newCost) {
 					dist[nextV] = newCost;
-					pq.add(new int[] {newCost, nextV});					
+					pq.add(new int[] {newCost, nextV});
 				}
 			}
 		}
-
+		
+//		for(int i = 1; i < n + 1; i++) {
+//			System.out.println(dist[i]);
+//		}
+		
 		PriorityQueue<int[]> pq2 = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-		pq2.add(new int[] {0, x});
+		// x에서 다른 곳으로 가는 길이
 		dist2[x] = 0;
+		pq2.add(new int[] {0, x});
+		
 		while(!pq2.isEmpty()) {
-			int num[] = pq2.poll();
-			int cost = num[0];
-			int v = num[1];
+			int cur[] = pq2.poll();
+			int cost = cur[0];
+			int v = cur[1];
 			
 			if(dist2[v] < cost) continue;
 			
@@ -76,21 +88,24 @@ public class Main{
 				int newCost = cost + nextCost;
 				if(dist2[nextV] > newCost) {
 					dist2[nextV] = newCost;
-					pq2.add(new int[] {newCost, nextV});					
+					pq2.add(new int[] {newCost, nextV});
 				}
 			}
 		}
-		int result = -1;
+		
+
+		int max_value = -1;
 		for(int i = 1; i < n + 1; i++) {
-			if(result < dist[i] +  dist2[i]) {
-				result = dist[i] + dist2[i];
+			if(dist[i] != INF && dist2[i] != INF && i != x) {
+				int sum = dist[i] + dist2[i];
+				if(max_value < sum) {
+					max_value = sum;
+				}
 			}
 		}
-		System.out.println(result);
+		System.out.println(max_value);
 		
 		
 		
-		
-				
 	}
 }
