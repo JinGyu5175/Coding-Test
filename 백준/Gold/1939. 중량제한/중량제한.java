5b70999a58b1f1cefbd2bfcc135aa241d608b933
Hong5175 <sony51755175@gmail.com>
2026-04-03 14:55:58 +0900

    [Gold III] Title: 중량제한, Time: 480 ms, Memory: 56960 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.java
+++ 백준/Gold/1939. 중량제한/중량제한.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,60 +8,60 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        List<long[]> graph[] = new ArrayList[n + 1];
-        long dist[] = new long[n + 1];
-
-        for(int i = 1; i < n + 1; i++){
-            graph[i] = new ArrayList<>();
-            dist[i] = -1;
-        }
-
-        for(int i = 0; i < m; i++){
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            long cost = Long.parseLong(st.nextToken());
-
-            graph[from].add(new long[]{cost, to});
-            graph[to].add(new long[]{cost, from});
-        }
-        st = new StringTokenizer(br.readLine());
-        int start = Integer.parseInt(st.nextToken());
-        int end = Integer.parseInt(st.nextToken());
-        PriorityQueue<long[]> pq = new PriorityQueue<>((a, b) ->Long.compare(b[0], a[0]));
-
-        pq.add(new long[]{Long.MAX_VALUE, start});
-        dist[start] = Long.MAX_VALUE;
-
-        while(!pq.isEmpty()){
-            long cur[] = pq.poll();
-            int v = (int)cur[1];
-            long cost = cur[0];
-
-            if(dist[v] > cost) continue;
-            
-            for(long[] next : graph[v]){
-                long nextCost = next[0];
-                int nextV = (int)next[1];
-
-
-                long newCost = Math.min(cost, nextCost);
-
-                if(dist[nextV] < newCost){
-                    dist[nextV] = newCost;
-                    pq.add(new long[]{newCost, nextV});
-                }
-            }
-        }
-
-        System.out.println(dist[end]);
-
-
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		List<long[]> graph[] = new ArrayList[n + 1];
+		long dist[] = new long[n + 1];
+		long INF = Long.MAX_VALUE / 4;
+		
+		for(int i = 1; i < n + 1; i++) {
+			graph[i] = new ArrayList<>();
+			dist[i] = -1;
+		}
+		
+		for(int i = 0 ; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			long cost = Integer.parseInt(st.nextToken());
+			graph[from].add(new long[] {cost, to});
+			graph[to].add(new long[] {cost, from});
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int start = Integer.parseInt(st.nextToken());
+		int end = Integer.parseInt(st.nextToken());
+		
+		PriorityQueue<long[]> pq = new PriorityQueue<>((a, b) -> Long.compare(b[0], a[0]));
+		
+		pq.add(new long[] {INF, start});
+		dist[start] = INF;
+		
+		while(!pq.isEmpty()) {
+			long cur[] = pq.poll();
+			long cost = cur[0];
+			int v = (int)cur[1];
+			
+			if(dist[v] > cost) continue; // 현재 다리까지의 무게보다 작으면 의미 없음(변화 없음)
+			
+			for(long next[] : graph[v]) {
+				long nextCost = next[0];
+				int nextV = (int)next[1];
+				
+				long newCost = Math.min(nextCost, cost);
+				
+				if(dist[nextV] < newCost) {
+					dist[nextV] = newCost;
+					pq.add(new long[] {newCost, nextV});
+				}
+			}
+		}
+		System.out.println(dist[end]);
+		
+	}
 }
