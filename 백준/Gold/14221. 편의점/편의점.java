0b2e3c9c81910e5685e96268ae16760aa702162b
Hong5175 <sony51755175@gmail.com>
2026-04-03 16:21:53 +0900

    [Gold III] Title: 편의점, Time: 612 ms, Memory: 68764 KB -BaekjoonHub

--- 백준/Gold/14221. 편의점/편의점.java
+++ 백준/Gold/14221. 편의점/편의점.java
@@ -11,15 +11,13 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		
 		List<int[]> graph[] = new ArrayList[n + 1];
-		int dist[] = new int[n + 1];
-		int INF = Integer.MAX_VALUE;
 		
 		for(int i = 1; i < n + 1; i++) {
-			dist[i] = INF;
 			graph[i] = new ArrayList<>();
 		}
 		
@@ -28,69 +26,75 @@ public class Main {
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
 			int cost = Integer.parseInt(st.nextToken());
-			
 			graph[from].add(new int[] {cost, to});
 			graph[to].add(new int[] {cost, from});
 		}
+		st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		int home[] = new int[a];
+		int shop[] = new int[b];
 		
 		st = new StringTokenizer(br.readLine());
-		int home_count = Integer.parseInt(st.nextToken());
-		int mart_count = Integer.parseInt(st.nextToken());
-		
-		int home[] = new int[home_count];
-		int mart[] = new int[mart_count];
-		
+ 		for(int i = 0; i < a; i++) {
+ 			int ot = Integer.parseInt(st.nextToken());
+ 			home[i] = ot;
+ 		}
+ 		
 		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < home_count; i++) {
-			home[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < mart_count; i++) {
-			mart[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		
-		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-		for(int i = 0; i < mart_count; i++) {
-			int ot = mart[i];
-			dist[ot] = 0;
-			pq.add(new int[] {0, ot});
-		}
-		
-		while(!pq.isEmpty()) {
-			int cur[] = pq.poll();
-			int cost = cur[0];
-			int v = cur[1];
-			
-			if(dist[v] < cost)continue;
-			
-			for(int next[] : graph[v]) {
-				int nextCost = next[0];
-				int nextV = next[1];
-				
-				int newCost = cost + nextCost;
-				
-				if(dist[nextV] > newCost) {
-					dist[nextV] = newCost;
-					pq.add(new int[] {newCost , nextV});
-				}
-			}
-		}
-		
-		int min_value = Integer.MAX_VALUE;
-		int result = -1;
-		for(int h : home) {
-			int cost = dist[h];
-			if(cost != 0 && cost < min_value) {
-				min_value = cost;
-				result = h;
-			}
-			else if(cost == min_value && h < result) {
-				result = h;
-			}
-		}
-		System.out.println(result);
-		
+ 		for(int i = 0; i < b; i++) {
+ 			int ot = Integer.parseInt(st.nextToken());
+ 			shop[i] = ot;
+ 		}
+ 		
+ 		int dist[] = new int[n + 1];
+ 		int INF = Integer.MAX_VALUE;
+ 		
+ 		for(int i = 1; i < n + 1; i++) {
+ 			dist[i] = INF;
+ 		}
+ 		
+ 		PriorityQueue<int[]> pq = new PriorityQueue<>((c, d) -> c[0] - d[0]);
+ 		for(int i = 0; i < b; i++) {
+ 			dist[shop[i]] = 0;
+ 			pq.add(new int[] {0, shop[i]});
+ 		}
+ 
+ 		while(!pq.isEmpty()) {
+ 			int cur[] = pq.poll();
+ 			int cost = cur[0];
+ 			int v = cur[1];
+ 			
+ 			if(dist[v] < cost) continue;
+ 			
+ 			for(int next[] : graph[v]) {
+ 				int nextCost = next[0];
+ 				int nextV = next[1];
+ 				
+ 				int newCost = nextCost + cost;
+ 				if(dist[nextV] > newCost) {
+ 					dist[nextV] = newCost;
+ 					pq.add(new int[] {newCost, nextV});
+ 				}
+ 			}
+ 		}
+ 		
+ 		int min_value = INF;
+ 		int target = Integer.MAX_VALUE;
+
+ 		for (int i = 0; i < a; i++) {
+ 		    int h = home[i];
+
+ 		    if (dist[h] < min_value) {
+ 		        min_value = dist[h];
+ 		        target = h;
+ 		    } else if (dist[h] == min_value && h < target) {
+ 		        target = h;
+ 		    }
+ 		}
+
+ 		System.out.println(target);
 	}
 }
