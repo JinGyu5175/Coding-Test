99c38f8a4c38e695f40c39e59b1fec1ff700f20c
Hong5175 <sony51755175@gmail.com>
2026-04-08 17:40:56 +0900

    [Gold IV] Title: 부분합, Time: 260 ms, Memory: 24088 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,49 +8,52 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		int s = Integer.parseInt(st.nextToken());
 		
 		int num[] = new int[n];
-		
 		st = new StringTokenizer(br.readLine());
 		
 		for(int i = 0; i < n; i++) {
 			num[i] = Integer.parseInt(st.nextToken());
 		}
-		int min_value = Integer.MAX_VALUE;
+		
 		int start = 0;
 		int end = 0;
 		int sum = num[start];
+		int min_value = Integer.MAX_VALUE;
 		
 		while(true) {
-//			System.out.println(start + " " + end + " " + sum);
-			if(sum < m) {
+			// 합이 작을때
+			if(sum < s) {
 				if(end + 1 < n) {
-					end += 1;
-					sum += num[end];				
+					end++;
+					sum += num[end];
 				}
 				else {
 					break;
 				}
 			}
-			else if(sum >= m) {
+			
+			// 합이 클때
+			else {
 				if(min_value > end - start) {
 					min_value = end - start;
 				}
+				
 				sum -= num[start];
 				start += 1;
+				
 				if(start > end) {
 					break;
 				}
 			}
 		}
+		
 		if(min_value == Integer.MAX_VALUE) {
 			System.out.println(0);
 		}
 		else {
-			System.out.println(min_value + 1);	
+			System.out.println(min_value + 1);
 		}
-		
-		
-	}
+ 	}
 }
