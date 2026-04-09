1ed65ab22f5b94b3528e26bb03cf2ac693475f22
Hong5175 <sony51755175@gmail.com>
2026-04-09 16:04:08 +0900

    [Gold III] Title: 팰린드롬 만들기, Time: 352 ms, Memory: 112560 KB -BaekjoonHub

--- 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -1,32 +1,39 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int nums[] = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++){
-            nums[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int dp[][] = new int[n][n];
-        for(int len = 2; len < n + 1; len++){
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int nums[] = new int[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < n; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int dp[][] = new int[n][n];
+		
+		for(int len = 2; len < n + 1; len++) {
             for(int start = 0; start + len - 1 < n; start++){
-                int end = start + len - 1;
-
-                if(nums[start] == nums[end]){
-                    if(len == 2) dp[start][end] = 0;
-                    else dp[start][end] = dp[start + 1][end - 1];
-                }
-                else{
-                    dp[start][end] = Math.min(dp[start + 1][end], dp[start][end - 1]) + 1;
-                }
+				int end = start + len - 1;
+				
+				if(nums[start] == nums[end]) {
+					if(len == 2) dp[start][end] = 0;
+					else {
+						dp[start][end] = dp[start + 1][end - 1];
+					}
+					
+				}
+				else {
+					dp[start][end] = Math.min(dp[start + 1][end], dp[start][end - 1]) + 1;
+				}
             }
-        }
-        System.out.println(dp[0][n - 1]);
-    }
+		}
+		System.out.println(dp[0][n - 1]);
+	}
 }
