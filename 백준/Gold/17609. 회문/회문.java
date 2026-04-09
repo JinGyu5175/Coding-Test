f70a0d99f14f56e139f05b2c6e06ba6c1e7f3908
Hong5175 <sony51755175@gmail.com>
2026-04-09 15:01:48 +0900

    [Gold V] Title: 회문, Time: 284 ms, Memory: 28740 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.java
+++ 백준/Gold/17609. 회문/회문.java
@@ -1,49 +1,54 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        for(int i = 0; i < n; i++){
-            String words = br.readLine();
-
-            boolean flag = false;
-            int s = 0;
-            int e = words.length() - 1;
-            while(s < e){
-                if(words.charAt(s) == words.charAt(e)) {
-                    s++;
-                    e--;
-                }
-                else{
-                    if(cal(words, s + 1, e) || cal(words, s, e - 1)){
-                        System.out.println(1);
-                        flag = true;
-                        break;
-                    }
-                    else{
-                        System.out.println(2);
-                        flag = true;
-                        break;
-                    }
-
-                }
-            }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		for(int i = 0; i < n; i++) {
+			String words = br.readLine();
+			
+			int start = 0;
+			int end = words.length() - 1;
+			boolean flag = false;
+			
+			while(start < end){
+				
+				if(words.charAt(start) == words.charAt(end)) {
+					start++;
+					end--;
+				}
+				else {
+					if(ispall(start + 1, end, words) || ispall(start, end - 1, words)) {
+						System.out.println(1);
+						flag = true;
+						break;
+					}
+					else {
+						System.out.println(2);
+						flag = true;
+						break;
+					}
+				}
+			}
             if(flag == false) System.out.println(0);
-
-        }
-    }
-
-    static boolean cal(String words, int s, int e){
-
-        while(s < e){
-            if(words.charAt(s) != words.charAt(e)) return false;
-            s++;
-            e--;
-        }
-        return true;
-    }
+		}
+				
+	}
+	static boolean ispall(int start, int end, String words) {
+		boolean flag = true;
+		
+		while(start < end) {
+			if(words.charAt(start) != words.charAt(end)) {
+				flag = false;
+				break;
+			}
+			start ++;
+			end --;
+		}
+		
+		return flag;		
+	}
 }
