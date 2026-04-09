ced6b47375b47fb0f947ab467b6d0b83c5f8e719
Hong5175 <sony51755175@gmail.com>
2026-04-09 16:17:45 +0900

    [Gold III] Title: 팰린드롬 만들기, Time: 356 ms, Memory: 112484 KB -BaekjoonHub

--- 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -19,7 +19,7 @@ public class Main {
 		int dp[][] = new int[n][n];
 		
 		for(int len = 2; len < n + 1; len++) {
-            for(int start = 0; start + len - 1 < n; start++){
+			for(int start = 0; start < n - len + 1; start++) {
 				int end = start + len - 1;
 				
 				if(nums[start] == nums[end]) {
@@ -27,12 +27,11 @@ public class Main {
 					else {
 						dp[start][end] = dp[start + 1][end - 1];
 					}
-					
 				}
 				else {
-					dp[start][end] = Math.min(dp[start + 1][end], dp[start][end - 1]) + 1;
+					dp[start][end] = Math.min(dp[start + 1][end], dp[start][end- 1]) + 1;
 				}
-            }
+			}
 		}
 		System.out.println(dp[0][n - 1]);
 	}
